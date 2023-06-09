5d7e7862e9902830f8eb408c0578d725d155645f
jeremyloh-hub <eziorain@gmail.com>
2023-06-09 14:53:41 +0800

    end of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,19 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => 
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
@@ -28,7 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
- 
+
 app.UseHttpsRedirection();
 
 app.UseCors("CorsPolicy");
@@ -48,9 +37,8 @@ try
 }
 catch (Exception ex)
 {
-    
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex , "An error occured during migration");
+    logger.LogError(ex, "An error occured during migration");
 }
 
 app.Run();
